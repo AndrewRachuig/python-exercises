29a8af9bfe324384fb5c49445de37eea446aacaf
Andrew Rachuig <102124490+AndrewRachuig@users.noreply.github.com>
2022-04-10 18:52:32 -0500

    updated and completed

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -27,35 +27,78 @@ print(uppper_cased_fruits)
 capitalized_fruits = [x.title() for x in fruits]
 print(capitalized_fruits)
 
+
 # Exercise 3 - Use a list comprehension to make a variable named fruits_with_more_than_two_vowels. Hint: You'll need a way to check if something is a vowel.
+import re
+fruits_with_more_than_two_vowels = [x for x in fruits if (len(re.split("[aeiou]", x)) > 3)]
 
 
 # Exercise 4 - make a variable named fruits_with_only_two_vowels. The result should be ['mango', 'kiwi', 'strawberry']
+fruits_with_only_two_vowels = [x for x in fruits if not (len(re.split("[aeiou]", x)) > 3)] 
+
 
 # Exercise 5 - make a list that contains each fruit with more than 5 characters
+fruits_with_more_than_five_chars = [x for x in fruits if (len(x) > 5)]
+
 
 # Exercise 6 - make a list that contains each fruit with exactly 5 characters
+fruits_with_exactly_five_chars = [x for x in fruits if (len(x) == 5)]
+
 
 # Exercise 7 - Make a list that contains fruits that have less than 5 characters
+fruits_with_less_than_five_chars = [x for x in fruits if (len(x) < 5)]
+
 
 # Exercise 8 - Make a list containing the number of characters in each fruit. Output would be [5, 4, 10, etc... ]
+number_of_characters_per_fruit = [len(x) for x in fruits]
+
 
 # Exercise 9 - Make a variable named fruits_with_letter_a that contains a list of only the fruits that contain the letter "a"
+fruits_with_letter_a = [x for x in fruits if "a" in x]
+
 
 # Exercise 10 - Make a variable named even_numbers that holds only the even numbers 
+even_numbers = [x for x in numbers if (x % 2 ==0)]
+
 
 # Exercise 11 - Make a variable named odd_numbers that holds only the odd numbers
+odd_numbers = [x for x in numbers if not (x % 2 ==0)]
+
 
 # Exercise 12 - Make a variable named positive_numbers that holds only the positive numbers
+positive_numbers = [x for x in numbers if x > 0]
+
 
 # Exercise 13 - Make a variable named negative_numbers that holds only the negative numbers
+negative_numbers = [x for x in numbers if x < 0]
+
 
 # Exercise 14 - use a list comprehension w/ a conditional in order to produce a list of numbers with 2 or more numerals
+two_numerals = [x for x in numbers if len(str(x)) > 1]
+
 
 # Exercise 15 - Make a variable named numbers_squared that contains the numbers list with each element squared. Output is [4, 9, 16, etc...]
+numbers_squared = [(x ** 2) for x in numbers]
+
 
 # Exercise 16 - Make a variable named odd_negative_numbers that contains only the numbers that are both odd and negative.
+odd_negative_numbers = [x for x in numbers if (x < 0) and (x % 2 != 0)]
+
 
 # Exercise 17 - Make a variable named numbers_plus_5. In it, return a list containing each number plus five. 
+numbers_plus_5 = [(x + 5) for x in numbers]
 
-# BONUS Make a variable named "primes" that is a list containing the prime numbers in the numbers list. *Hint* you may want to make or find a helper function that determines if a given number is prime or not.
+
+# BONUS Make a variable named "primes" that is a list containing the prime numbers in the numbers list. 
+# *Hint* you may want to make or find a helper function that determines if a given number is prime or not.
+def isprime(num):
+    if num < 0:
+        return False
+    else:
+        for n in range(2, int(num ** 0.5) + 1):
+            if num % n == 0:
+                return False
+        return True
+
+
+primes = [x for x in numbers if isprime(x)]
